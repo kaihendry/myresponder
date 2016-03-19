cb62c974a8b1cf936dea928dc7a65ff1ca704acd
root <hendry@iki.fi>
2016-03-19 08:54:39 +0000

    Move to SSL

--- h/alert.php
+++ h/alert.php
@@ -4,6 +4,7 @@ require_once("../config.php");
 
 function e( $text ){ return htmlspecialchars( $text, ENT_QUOTES, 'UTF-8' ); }
 function je( $obj ){ return json_encode($obj, JSON_PRETTY_PRINT); }
+// Malaysian numbers
 function my_number( $n ){ return (substr($n, 0, 1) == 0) ? "6$n" : $n; }
 
 function alert($id) {
@@ -27,7 +28,7 @@ function alert($id) {
 	foreach (glob("../g/r/*.json") as $gj) {
 		$g = json_decode(file_get_contents($gj), true);
 		// https://docs.nexmo.com/api-ref/sms-api/request
-		$url = $URL . "&from=MYRESP&callback=http://h.$HOST/report/&status-report-req=1&type=unicode&to=" . my_number($g["tel"]) .
+		$url = $URL . "&from=MYRESP&callback=https://h.$HOST/report/&status-report-req=1&type=unicode&to=" . my_number($g["tel"]) .
 			"&text=" . urlencode("⚠" . $_SESSION["address"] . " tel:" . $_SESSION["tel"] . " " . $_SESSION["name"] . " at " . date("c", $ts));
 		curl_setopt($ch, CURLOPT_URL, $url);
 
@@ -75,7 +76,7 @@ if (empty($_GET["ic"])) { // We need to ensure we are always a GET request to en
 		'name'=> $_SESSION["name"],
 		'address'=> $_SESSION["address"],
 		'tel'=> $_SESSION["tel"]);
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . "/alert.php?" . http_build_query($data);
+	$url = 'https://' . $_SERVER['HTTP_HOST'] . "/alert.php?" . http_build_query($data);
 	header('Location: ' . $url);
 }
 
